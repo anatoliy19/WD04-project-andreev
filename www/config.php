5c49d5399118d21acb6aa800d62ba973a1d3976f
Anatoly <AnatoliiAndreev19@gmail.com>
2018-10-02 13:21:52 +0300

    Настройка проекта

---
+++ www/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+// DB Settings
+define('DB_HOST', 'wd04-project-andreev');
+define('DB_NAME', 'wd04-project-andreev');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Хост сайта
+define('HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+
+// Физический путь к корневой директории скрипта
+define('ROOT', dirname(__FILE__).'/');
+
+
+?>
